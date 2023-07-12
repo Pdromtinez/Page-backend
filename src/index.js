61a94d8fb2bba1ccaf86da7eb240dfe8736290b3
Pdromtinez <137045122+Pdromtinez@users.noreply.github.com>
2023-07-12 22:03:15 +0200

    hola server

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
+PORT = 3000;
+
 const Router = require("@koa/router");
 const Koa = require("koa");
 const { default: koaBody } = require("koa-body");
 const KoaLogger = require("koa-logger");
-const db = require("./models");
 const cors = require("@koa/cors");
+const app = new Koa();
+const router = new Router();
+
+router.get("/", (ctx, next) => {
+    ctx.response.body = { message: "Hello world!" };
+    ctx.status = 200;
+});
 
 app.use(KoaLogger());
 app.use(cors({ 
@@ -11,4 +19,9 @@ app.use(cors({
     , methods : ["GET", "POST, PUT"]}));
 app.use(koaBody());
 
+
 app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(PORT, () => {
+    console.log(`app listening on port: ${PORT}`);
+});
