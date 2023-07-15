229edd3454eb1b7a5f67e316c473448295a0df0f
Pdromtinez <137045122+Pdromtinez@users.noreply.github.com>
2023-07-15 14:22:18 +0200

    prueba post

--- src/index.js
+++ src/index.js
@@ -5,14 +5,29 @@ const Koa = require("koa");
 const { default: koaBody } = require("koa-body");
 const KoaLogger = require("koa-logger");
 const cors = require("@koa/cors");
+
 const app = new Koa();
 const router = new Router();
+const datosArray = []
 
 router.get("/", (ctx, next) => {
     ctx.response.body = { message: "Hello world!" };
     ctx.status = 200;
 });
 
+router.post('/agregar-datos', (ctx) => {
+    const datos = JSON.stringify(ctx.request.body);
+
+    datosArray.push(datos);
+
+    ctx.body = { message: 'Datos agregados correctamente' };
+});
+
+router.get('/datos', (ctx) => {
+    ctx.body = datosArray;
+});
+
+app.use(bodyParser());
 app.use(KoaLogger());
 app.use(cors({ 
     origin: "*" 
