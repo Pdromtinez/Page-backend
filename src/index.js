903b6d160be3c415a71f4bb3570405c4f6511a58
Pdromtinez <137045122+Pdromtinez@users.noreply.github.com>
2023-08-31 13:52:01 +0200

    nuevo intento import

--- src/index.js
+++ src/index.js
@@ -1,70 +1,64 @@
-PORT = 3000;
+import fs from 'fs/promises';
+import Router from '@koa/router';
+import Koa from 'koa';
+import koaBody from 'koa-body';
+import KoaLogger from 'koa-logger';
+import cors from '@koa/cors';
 
-const Router = require("@koa/router");
-const Koa = require("koa");
-const { default: koaBody } = require("koa-body");
-const KoaLogger = require("koa-logger");
-const cors = require("@koa/cors");
 const app = new Koa();
-
 const router = new Router();
+const datosFronted = [];
 
-
-const datosArray = []
-
-
-//----------------------------------------------------------------------------------
-router.get("/", (ctx, next) => {
-    ctx.response.body = { message: "Hello world!" };
-    ctx.status = 200;
+router.get('/', (ctx, next) => {
+  ctx.response.body = { message: 'Hello world!' };
+  ctx.status = 200;
 });
 
-router.post('/agregar-datos', (ctx) => {
-  const nuevosDatos = ctx.request.body;
+router.post('/Fronted', async (ctx) => {
+  const nuevosDatosFronted = ctx.request.body;
 
-  if (!Array.isArray(nuevosDatos)) {
-  
-    nuevosDatos = [nuevosDatos];
+  try {
+    // Verificar si tienes permisos de escritura en la carpeta
+    await verificarPermisos();
+
+    // Agregar los nuevos datos a datosFronted
+    datosFronted.push(nuevosDatosFronted);
+
+    // Guardar los datos en el archivo
+    await guardarDatosFrontedEnArchivo();
+
+    ctx.body = { message: 'Datos de Fronted agregados correctamente' };
+  } catch (error) {
+    console.error('Error al guardar los datos de Fronted:', error);
+    ctx.body = { message: 'Error al guardar los datos de Fronted' };
   }
-
-  
-  datosArray.length = 0;
-
-  
-  nuevosDatos.forEach(objeto => {
-    datosArray.push(objeto);
-  });
-
-  ctx.body = { message: 'Datos agregados correctamente' };
 });
 
+async function verificarPermisos() {
+  try {
+    await fs.access(__dirname, fs.constants.W_OK);
+  } catch (err) {
+    throw new Error('No tienes permisos de escritura en esta carpeta.');
+  }
+}
 
-router.post('/agregar-datosFormulario', (ctx) => {
-  const datos = ctx.request.body;
-  
-  datosArray.push(datos);
-    
+async function guardarDatosFrontedEnArchivo() {
+  const datosJSON = JSON.stringify(datosFronted, null, 2);
 
-
-  ctx.body = { message: 'Datos agregados correctamente' };
-});
-
-router.get('/datos', (ctx) => {
-    ctx.response.type = 'application/json'; 
-    ctx.response.body = datosArray; 
-});
-
+  try {
+    await fs.writeFile('datosFronted.json', datosJSON);
+    console.log('Datos de Fronted guardados en el archivo JSON');
+  } catch (err) {
+    throw new Error('Error al guardar los datos de Fronted en el archivo JSON');
+  }
+}
 
 app.use(KoaLogger());
-app.use(cors({ 
-    origin: "*" 
-    , methods : ["GET", "POST, PUT"]}));
+app.use(cors({ origin: '*', methods: ['GET', 'POST'] }));
 app.use(koaBody());
-
-
 app.use(router.routes()).use(router.allowedMethods());
 
+const PORT = 3000;
 app.listen(PORT, () => {
-    console.log(`app listening on port: ${PORT}`);
+  console.log(`App listening on port: ${PORT}`);
 });
-
--- src/start.js
+++ src/start.js
@@ -1,19 +1,23 @@
-const util = require("util");
-const exec = util.promisify(require("child_process").exec);
+import { promisify } from "util";
+import { exec } from "child_process";  // Importar directamente de child_process
+const execPromise = promisify(exec);
 
 async function deploy() {
-    const { stdout: output1 } = await exec("yarn install");
+  try {
+    const { stdout: output1 } = await execPromise("yarn install");
     console.log("Installing dependencies...");
     console.log(output1);
 
-    const { stdout: output2 } = await exec("yarn db:migrate");
+    const { stdout: output2 } = await execPromise("yarn db:migrate");
     console.log("Migrating database...");
     console.log(output2);
 
-    const { stdout: output3 } = await exec("yarn db:seed");
+    const { stdout: output3 } = await execPromise("yarn db:seed");
     console.log("Seeding database...");
     console.log(output3);
+  } catch (error) {
+    console.error("An error occurred:", error);
+  }
 }
-
 
 deploy();
